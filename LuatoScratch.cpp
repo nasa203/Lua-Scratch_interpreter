51c26f8ae7ebee9b110b33750ddc6b70a7821e12
jaydenlin2019@gmail.com <137369573+nasa203@users.noreply.github.com>
2026-07-04 19:53:09 -0700

    starting on the lexer ig

--- LuatoScratch.cpp
+++ LuatoScratch.cpp
@@ -12,3 +12,29 @@ struct Token{
     TokenType type;
     string value;
 };
+
+class Sprite{
+public:
+    string name;
+    double x = 0.0;
+    double y = 0.0;
+    double size = 100.0;
+    double direction = 90.0;
+    unordered_map<string, string> local_variables;
+    Sprite(string sprite_name) : name(sprite_name) {}
+    virtual ~Sprite() = default;
+    virtual void update() = 0;
+};
+
+class lexer{
+private:
+    size_t pos;
+    string src;
+    char peek(){
+
+    }
+    char advance(){
+        
+    }
+public:
+};
