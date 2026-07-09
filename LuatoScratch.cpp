f15f08a609c6c8a81bda41a43f85c7f38df7e209
jaydenlin2019@gmail.com <137369573+nasa203@users.noreply.github.com>
2026-07-08 17:14:18 -0700

    added stupid block ids and random generating

--- LuatoScratch.cpp
+++ LuatoScratch.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <random>
+#include <ctime>
 #include "json.hpp"
 using namespace std;
 using json = nlohmann::json;
@@ -30,10 +32,19 @@ public:
     double y = 0.0;
     double size = 100.0;
     double direction = 90.0;
+    vector<Block> blocks;
     unordered_map<string, string> local_variables;
     Sprite(string sprite_name) : name(sprite_name) {}
     virtual ~Sprite() = default;
     virtual void update() = 0;
+};
+
+class Block{
+    string id;
+    string opcode;
+    string next_id = "";
+    string parent_id = "";
+    vector<string> inputs;
 };
 
 //==========PREPROCESSING==========//
@@ -42,6 +53,97 @@ class lexer{
 private:
     size_t pos;
     string src;
+    size_t blocks = 0;
+    vector<string> funny_blocks = {
+        "block_of_regret",
+        "im_just_a_block_standing_here",
+        "sentient_but_lazy_block",
+        "block_that_should_have_been_a_sphere",
+        "error_404_block_not_found",
+        "unpaid_intern_block",
+        "emotional_support_block",
+        "block_with_existential_dread",
+        "congratulations_its_a_block",
+        "please_dont_delete_this_block",
+        "block_i_made_instead_of_sleeping",
+        "load_bearing_cardboard_block",
+        "just_another_block"
+        "schrodingers_block"
+        "definitely_a_block"
+        "block_placeholder_final_v2"
+        "unidentified_flying_block"
+        "not_a_cube_just_a_block"
+        "block_that_could_have_been_an_email",
+        "i_have_no_idea_what_this_block_does",
+        "blocky_mcblockface",
+        "look_mom_i_made_a_block",
+        "block_with_commitment_issues",
+        "mediocre_block_at_best",
+        "procrastination_block_alpha",
+        "this_block_is_judging_you",
+        "barely_functioning_block",
+        "block_holding_the_whole_app_together",
+        "not_my_problem_block",
+        "spaghetti_code_block_v3",
+        "block_asking_for_a_friend",
+        "definitely_not_three_blocks_in_a_trenchcoat",
+        "unwarranted_confidence_block",
+        "block_under_qualified_for_this_job",
+        "temporary_block_since_2022",
+        "you_spin_me_right_round_baby_like_a_block",
+        "block_experiencing_burnout",
+        "just_trust_me_it_is_a_block",
+        "passive_aggressive_block",
+        "block_trying_its_best_okay",
+        "it_works_on_my_machine_block",
+        "the_chosen_block",
+        "sweet_dreams_are_made_of_blocks"
+        "block_that_refuses_to_elaborate",
+        "glorified_cube_id",
+        "this_block_requires_coffee",
+        "shhh_the_block_is_sleeping",
+        "block_with_imposter_syndrome",
+        "somebody_save_this_block",
+        "block_highly_likely_to_break",
+        "absolute_unit_of_a_block",
+        "block_is_over_it",
+        "ctrl_c_ctrl_v_block",
+        "block_manifesting_success",
+        "legacy_block_nobody_dares_touch",
+        "block_hiding_from_the_compiler",
+        "wild_block_appeared",
+        "block_of_netherite"
+        "unsolicited_block_pic",
+        "block_running_on_vibes_alone",
+        "expensive_way_to_store_nothing_block",
+        "block_suffering_from_success",
+        "you_had_one_job_block",
+        "block_complaining_about_the_weather",
+        "is_it_a_block_is_it_a_plane",
+        "block_that_knows_too_much",
+        "parental_guidance_suggested_block",
+        "final_final_actual_final_block"
+        "lukewarm_block_of_tofu",
+        "overpriced_artisanal_block",
+        "petrified_wood_block",
+        "chunk_of_expired_cheddar_block",
+        "solid_lead_block_do_not_lift",
+        "glowing_radioactive_uranium_block",
+        "greasy_mechanics_engine_block",
+        "frozen_tundra_dirt_block",
+        "compressed_lint_block",
+        "dense_neutron_star_block",
+        "slippery_soap_block",
+        "shattered_concrete_cinder_block",
+        "block_made_entirely_of_smaller_blocks"
+        "waxed_lightly_weathered_cut_copper_block"
+    };
+    string gen_unique_id(){
+        mt19937 rng(static_cast<unsigned int>(time(nullptr)));
+        uniform_int_distribution<int> dist(0, funny_blocks.size() - 1);
+        int random_index = dist(rng);
+        return funny_blocks[random_index] + "_" + to_string(blocks);
+    }
     char peek(){
         if (pos >= src.size()) return '\0';
         return src[pos];
